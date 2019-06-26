be894d98d3317c57fbdf16ccdac79b199a891944
Gustavo Possebon Machado <gustavo.possebon@dbccompany.com.br>
2019-06-26 15:02:10 -0300

    feat(Elfo,Item): criar construtor para ambas e métodos utilitários

--- modulo-01/Elfo.java
+++ modulo-01/Elfo.java
@@ -1,6 +1,12 @@
 public class Elfo{
     private String nome;
+    private Item flecha = new Item(4, "Flecha");
+    private Item arco = new Item(1, "Arco");
     private int experiencia = 0;
+    
+    public Elfo(String nome){
+        this.nome = nome;
+    }
     
     public String getNome(){
         return this.nome;
@@ -14,7 +20,23 @@ public class Elfo{
         return this.experiencia;
     }
     
-    public void setExperiencia(int experiencia){
-        this.experiencia = experiencia;
+    public Item getFlecha(){
+        return this.flecha;
+    }
+    
+    public int getQtdFelcha(){
+        return this.flecha.getQuantidade();
+    }
+    
+    public void aumentarXp(){
+        this.experiencia++;
+    }
+    
+    public void atirarFlechar(){
+        int qtdAtual = this.flecha.getQuantidade();
+        if(qtdAtual > 0){
+            flecha.setQuantidade(qtdAtual - 1);
+        } 
+        this.aumentarXp();
     }
 }

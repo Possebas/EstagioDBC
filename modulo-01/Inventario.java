eaaef4df78085b612e8014445c77d3371ec33fb3
Gustavo <gustavo.possebon@acad.pucrs.br>
2019-06-29 17:07:22 -0300

    feat(Inventario,InventarioTest): novas funcionalidades como ordenação em asc e desc

--- modulo-01/Inventario.java
+++ modulo-01/Inventario.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Inventario
 {
     private ArrayList<Item> itens;
-    private int count = 0;
     
     public Inventario(){
         itens = new ArrayList<>();
@@ -12,6 +11,79 @@ public class Inventario
     public ArrayList<Item> getItens(){
         return this.itens;
     }
-}
+   
+    public int getTamanho(){
+        return this.itens.size();
+    }
+    
+    public Item buscaItemPorDescricao(String descricao){
+        Item aux = null;
+        for(int i = 0; i < itens.size(); i++){
+            aux = itens.get(i);
+            if(aux.getDescricao() == descricao){
+               return aux;
+            }        
+        }      
+        return (aux != null) ? aux : null;
+    }
+    
+    public ArrayList<Item> inverter() { 
+        ArrayList<Item> aux = new ArrayList<Item>(); 
+        int tam = getTamanho() - 1;
+        for (int i = tam; i >= 0; i--){
+            aux.add(getItens().get(i)); 
+        }
+        return aux; 
+    } 
+    
+    public void ordenarItens(){ //Classico bubbleSort como otimização de n ao quadrado, considerando n como numero de elementos.
+       Item aux = null;
+       if (itens.size()>1){ // nem entra senão houver mais de um item
+            for (int j=0; j<itens.size(); j++){ //laço mais externo onde
+                for (int i=0; i < itens.size()-i; i++){
+                    if (itens.get(i).compareTo(itens.get(i+1)) > 0){ //comparo o que to, é maior que o proximo e então realizo a troca
+                        aux = itens.get(i); //atual fica em aux
+                        itens.set(i,itens.get(i+1) ); //proxima posição na atual
+                        itens.set(i+1, aux); //proxima posição com o aux (atual)
+                    }
+                }
+            }
+       } 
+    }
+    
+    public void ordenarItens(TipoOrdenacao ordem){ 
+       //Classico bubbleSort como otimização de n ao quadrado, considerando n como numero de elementos.
+       Item aux = null;
+       if(ordem == TipoOrdenacao.ASC){
+           if (itens.size()>1){ // nem entra senão houver mais de um item
+               for (int j=0; j<itens.size(); j++){ //laço mais externo onde
+                   for (int i=0; i < itens.size()-i; i++){
+                       if (itens.get(i).compareTo(itens.get(i+1)) < 0){ //comparo o que to, é menor que o proximo e então realizo a troca
+                           aux = itens.get(i); //atual fica em aux
+                           itens.set(i,itens.get(i+1) ); //proxima posição na atual
+                           itens.set(i+1, aux); //proxima posição com o aux (atual)
+                    }
+                }
+               }
+           }     
+        } //else{ordenarItens();}       
+    } 
+    
+    
+    public String imprimeNomeItens(){
+        StringBuilder resultado = new StringBuilder();
+        Item aux = null;
+        for(int i = 0; i < itens.size(); i++){
+            aux = itens.get(i);
+            if(aux != null){
+                resultado.append(aux.getDescricao());
+                resultado.append(",");
+            }
+        }   
+        return (resultado.length()>0 ? 
+                resultado.substring(0,(resultado.length()-1)) : 
+                resultado.toString());
+    }
+}   
     
    
--- modulo-01/InventarioTest.java
+++ modulo-01/InventarioTest.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -12,7 +13,60 @@ public class InventarioTest
         inventario.getItens().add(new Item(4,"Faca"));
         inventario.getItens().add(sabre);
         inventario.getItens().add(new Item(10,"Arma"));
-        
         assertEquals(sabre,inventario.getItens().get(1));
+    }
+    
+    @Test
+    public void buscaItemPorDescricao(){
+        Inventario inventario = new Inventario();
+        inventario.getItens().add(new Item(4,"Faca"));
+        inventario.getItens().add(new Item(1,"Adaga"));
+        inventario.getItens().add(new Item(5,"Sabre"));
+        assertEquals("Adaga",inventario.buscaItemPorDescricao("Adaga").getDescricao());
+    }
+    
+    @Test
+    public void inverteArrayComparaIgual(){
+        Inventario inventario = new Inventario();
+        inventario.getItens().add(new Item(4,"Faca"));
+        inventario.getItens().add(new Item(5,"Sabre"));
+        inventario.getItens().add(new Item(1,"Adaga"));
+        inventario.getItens().add(new Item(3,"Arma"));
+        ArrayList<Item> aux = inventario.inverter();
+        ArrayList<Item> desk = new ArrayList<>();
+        desk.add(new Item(3,"Arma"));
+        desk.add(new Item(1,"Adaga"));
+        desk.add(new Item(5,"Sabre"));
+        desk.add(new Item(4,"Faca"));
+        assertFalse(desk.equals(aux));
+    }
+    
+    @Test
+    public void imprimeNomeDosItens(){
+        Inventario inventario = new Inventario();
+        inventario.getItens().add(new Item(4,"Faca"));
+        inventario.getItens().add(new Item(1,"Adaga"));
+        inventario.getItens().add(new Item(5,"Sabre"));
+        assertEquals("Faca,Adaga,Sabre",inventario.imprimeNomeItens());
+    }
+    
+    @Test
+    public void imprimeNomeDosItensOrdenadoASC(){
+        Inventario inventario = new Inventario();
+        inventario.getItens().add(new Item(8, "Escudo de metal"));
+	inventario.getItens().add(new Item(3, "Poção de HP"));
+	inventario.getItens().add(new Item(5, "Bracelete"));
+	inventario.ordenarItens();
+        assertEquals("Poção de HP,Bracelete,Escudo de metal",inventario.imprimeNomeItens());
+    }
+    
+    @Test
+    public void imprimeNomeDosItensOrdenadoDESC(){
+        Inventario inventario = new Inventario();
+        inventario.getItens().add(new Item(6,"Sabre"));
+        inventario.getItens().add(new Item(8, "Escudo de metal"));
+	inventario.getItens().add(new Item(5, "Bracelete"));
+	inventario.ordenarItens(TipoOrdenacao.ASC);
+        assertEquals("Escudo de metal,Sabre,Bracelete",inventario.imprimeNomeItens());
     }
 }

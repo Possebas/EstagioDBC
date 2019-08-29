224a43932fa09ee7f9774f7369dc126386813def
Gustavo Possebon Machado <gustavo.possebon@dbccompany.com.br>
2019-08-29 16:48:49 -0300

    feat(modulo04): adicionar spring com maven

---
+++ modulo-04/springVemSer/src/main/java/com/example/springVemSer/SpringVemSerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springVemSer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVemSerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVemSerApplication.class, args);
+	}
+
+}
